f9395172f77046fc021bea93c20ccf700dd70496
Swati <jswati603@gmail.com>
2024-03-28 12:19:00 +0530

    drag and drop implemented

--- script1.js
+++ script1.js
@@ -5,9 +5,12 @@ let todoContainer=document.querySelector("#todo");
 
 
 addCardBtn.addEventListener("click",addTask);
+let count=1000000
 
 function addTask(){
     let card=document.createElement("div");
+    //add id
+    card.id=`card-${count++}`
     card.className="card"
     card.innerText="Test Card";
     //contenteditable
@@ -23,16 +26,63 @@ card.focus()
 //drag start
 card.addEventListener("dragstart",(eventDetails)=>{
     let draggedCard = eventDetails.target
+
     //we store unique of that element
 
     eventDetails.dataTransfer.setData("text/plain" ,draggedCard.id)
-    draggedCard.style.opacity=0.5;
+    draggedCard.style.opacity=0.5;// element is partially visible
+    // add data first:
+    // eventDetails.dataTransfer.setData("text/plain",draggedCard.id)
 })
 
 // dragend
 card.addEventListener("dragend",(eventDetails)=>{
     let draggedCard=eventDetails.target
-    draggedCard.style.opacity = 1;
+    draggedCard.style.opacity = 1; // it means element is clearly visible
 })
 
+// these 3 events are for sections.
+
+//dragover-from drag start to drag end
+//dragenter-once it enters the drag zone
+//drop-when it moved to its final destination
+
+// let todo=document.querySelector("todo");
+// let progress=document.querySelector("progress");
+// let done=document.querySelector("#done");
+
+// todo.addEventListener("dragover",(eventDetails)=>{
+//     eventDetails.preventDefault();
+// })
+
+// todo.addEventListener("dragenter",(eventDetails)=>{
+//     eventDetails.preventDefault();
+// })
+
+// todo.addEventListener("drop",(eventDetails)=>{
+//     eventDetails.preventDefault();
+// })
+
+//forEach loop
+
+
+let dragevents=["dragover","dragenter","drop"];
+
+dragevents.forEach((dropEvent)=>{
+
+    let  columns=document.querySelectorAll(".column");
+    for(let c of columns){        //todo,progress,done
+        c.addEventListener(dropEvent,(eventDetails)=>{
+            eventDetails.preventDefault()
+
+            if (dropEvent=="drop"){
+                //get the id of the card that has been dragged here.
+               let cardID=eventDetails.dataTransfer.getData("text/plain");
+               let draggedCard=document.querySelector(`#${cardID}`);
+               let currentColumn=eventDetails.target
+               currentColumn.append(draggedCard);
+   }
+  })
+ }
+ })
 }
